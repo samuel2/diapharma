a98c93c606a73d968ce367770abcb935326ec79d
samuel kamdoum folong <smlfolong@gmail.com>
2015-04-10 13:09:22 +0100

    archive.php

---
+++ archive.php
@@ -0,0 +1,77 @@
+<?php
+
+get_header();
+
+	if (have_posts()) :
+            ?>
+
+            <h2>
+                <?php 
+                
+                  if (is_category()) {
+                      single_cat_title();
+                  } elseif (is_tag()) {
+                      single_tag_title();
+                  } elseif (is_author()) {
+                      the_post();
+                      echo 'Author Archives : '.get_the_author();
+                      rewind_posts();
+                  } elseif (is_day()) {
+                      echo 'Archives du : '.get_the_date();
+                  } elseif (is_month()) {
+                      echo 'Archives du mois de : '.get_the_date('F Y');
+                  } elseif (is_year()) {
+                      echo 'Archives de l\'année : '.get_the_date(Y);
+                  } else {
+                      echo 'Archives:';
+                  }
+                
+                ?>
+            </h2>
+            
+            <?php 
+		while (have_posts()) : the_post(); ?>
+
+		    <article class="post">
+			<h2>
+                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+			</h2>
+                        
+                        <p class="post-info">
+                            <?php the_time('F jS, Y g:i a'); ?> | Posté par 
+                            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+                                <?php the_author(); ?>
+                            </a> dans | 
+                            
+                            <?php 
+                            
+                                $categories = get_the_category();
+                                $separator = ", ";
+                                $output = '';
+                                
+                                if ($categories) {
+                                    
+                                    foreach ($categories as $category) {
+                                        $output .= '<a href="' . get_category_link($category->term_id) .'">' . $category->cat_name . '</a>' . $separator;
+                                    }
+                                    
+                                    echo trim($output, $separator);
+                                }
+                            
+                            ?>
+                            
+                        </p>
+
+			<?php the_excerpt(); ?>
+		    </article>
+
+		<?php endwhile;
+	
+	else :
+	    echo '<P>Content not found</p>';
+	endif;
+
+get_footer();
+
+?>
+    
